ceea10f19a26b46aa962a0cb56da88eb30954cc7
SgtGarro <marcelogarro137@gmail.com>
2024-09-15 15:25:15 -0500

    feat (US-08): add login view for users

--- app/src/main/java/com/example/trabajomovil/LoginActivity.kt
+++ app/src/main/java/com/example/trabajomovil/LoginActivity.kt
@@ -1,28 +1,22 @@
 package com.example.trabajomovil
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        startLoginActivity()
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    private fun startLoginActivity() {
-        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
