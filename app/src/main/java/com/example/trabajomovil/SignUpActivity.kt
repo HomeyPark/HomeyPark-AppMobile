66d832c7476c753456baf319e8543eb61ee98234
SgtGarro <marcelogarro137@gmail.com>
2024-09-15 15:42:04 -0500

    feat (US-08): add signup view for users and connect it with login view

--- app/src/main/java/com/example/trabajomovil/SignUpActivity.kt
+++ app/src/main/java/com/example/trabajomovil/SignUpActivity.kt
@@ -1,33 +1,20 @@
 package com.example.trabajomovil
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-
-
-        addSignUpButtonActivity()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginMain)) { v, insets ->
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signUpMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-
-    private fun addSignUpButtonActivity() {
-        val signUpBtn = findViewById<MaterialButton>(R.id.loginMain_goToSignUp)
-
-        signUpBtn.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
