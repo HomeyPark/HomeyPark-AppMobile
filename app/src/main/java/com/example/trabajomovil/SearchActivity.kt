0404bd3a9c501ac78dde2a4f50c2287e5ccae445
VittorioEdu <u20231b547@upc.edu.pe>
2024-09-19 19:11:17 -0500

    feat: Add Parking Reservation View

--- app/src/main/java/com/example/trabajomovil/SearchActivity.kt
+++ app/src/main/java/com/example/trabajomovil/SearchActivity.kt
@@ -1,27 +1,20 @@
 package com.example.trabajomovil
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        startSearchActivity()
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_search)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.searchMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    private fun startSearchActivity(){
-        startActivity(Intent(this, SearchActivity::class.java))
     }
 }
