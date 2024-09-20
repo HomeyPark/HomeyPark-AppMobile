ed4efaa44c880ed676e531a85caf6c0a702c62e6
VittorioEdu <u20231b547@upc.edu.pe>
2024-09-20 16:50:40 -0500

    feat: Add Reservation History View

--- app/src/main/java/com/example/trabajomovil/HistoryActivity.kt
+++ app/src/main/java/com/example/trabajomovil/HistoryActivity.kt
@@ -1,30 +1,20 @@
 package com.example.trabajomovil
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class SearchActivity : AppCompatActivity() {
+class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.searchMain)) { v, insets ->
+        setContentView(R.layout.activity_history)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.historyMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        addBookNowHandler()
-    }
-    private fun addBookNowHandler() {
-        val bookNowBtn = findViewById<MaterialButton>(R.id.searchMain_bookNow)
-
-        bookNowBtn.setOnClickListener {
-            startActivity(Intent(this, HistoryActivity::class.java))
         }
     }
 }
